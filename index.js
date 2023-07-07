ca6059a724ccb89de80b4baf44700a214268d46d
BryceLingle <brycealingle@gmail.com>
2023-07-07 14:08:59 -0600

    prompts are created along with choices

--- index.js
+++ index.js
@@ -2,3 +2,53 @@
 //It doesn't break when running it so everythingis setup correctly to move forward
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+// Prompting user for information 
+inquirer.prompt([
+    {
+       message:'Enter the title of your project',
+       name:'title',
+       type:'input',
+    },
+    {
+        message:'Enter the description of your project',
+        name:'description',
+        type:'input',
+     },
+     {
+        message:'',
+        name:'installation',
+        type:'input',
+     },
+     {
+        message:'',
+        name:'usage',
+        type:'input',
+     },
+     {
+        message:'',
+        name:'contributing',
+        type:'input',
+     },
+     {
+        message:'',
+        name:'tests',
+        type:'input',
+     },
+     {
+        message:'Choose a license for your application:',
+        name:'license',
+        type:'list',
+        choices:['MIT', 'Apache 2.0', 'GPL 3.0', 'BSD 3-Clause', 'None'],
+     },
+     {
+        message:'Enter your GitHub username:',
+        name:'github',
+        type:'input',
+     },
+     {
+        message:'Enter your email address',
+        name:'email',
+        type:'input',
+     },
+])
