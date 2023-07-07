e15fee0737ea342afd7183cc5f8fd985e2152d6e
BryceLingle <brycealingle@gmail.com>
2023-07-07 13:57:07 -0600

    put in my file system and my inquirer to get input

---
+++ index.js
@@ -0,0 +1,4 @@
+//Here aremy two modules needed forthis assignment
+//It doesn't break when running it so everythingis setup correctly to move forward
+const inquirer = require('inquirer');
+const fs = require('fs');
